9fdac232d68f9411916eb1e112e1b2e2d3119482
leemgzu <92341784+leemgzu@users.noreply.github.com>
2023-09-28 14:40:31 -0700

    week4 update
    school notes

--- week3/index.js
+++ week3/index.js
@@ -1,43 +1,34 @@
-// const express = require('express');
-// const app = express()
-
-// const hostname = 'localhoat';
-// const port = 8080;
-
-// // const server = http.createServer((req, res) => {
-// //     res.statusCode = 200;
-// //     res.setHeader('Content-Type', 'text/plain');
-// //     res.end('Hello World');
-// // });
-
-// app.get('/hello', (request, response) => {
-//     console.log(request, "request")
-// });
-
-// // server.listen(port, hostname, () => {
-// //     console.log('Server running at http://${hostname}:')
-// // });
-// app.listen(port, () => {
-//     console.log('Server running' );
-// });
-
-//  this is a https web-server
 const https = require('https');
+const fs = require('fs');
 const express = require('express');
+const startUp = require('./routes/startup');
+
 const port = 8080
+const httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+}
+
 
 app = express();
 
-const fs = require('fs');
-const httpsOptions = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-}
 const server = https.createServer(httpsOptions,app);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}  );
+app.use(express.json());
+app.use('/', startUp)
+
+app.get('/get-device', (req, res) => {
+  const whatDeviceIsThis = req.header('User-Agent');
+  // if the device is Windows, send a flag: isWindows = true
+  // if the device is Mac, send a flag: isMac = true
+  // if the device is linux, send a flag: is Linus = true
+  res.send({whatDeviceIsThis});
+});
+
+app.get('/get-ip', (req, res) => {
+  const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  res.send({userIp});
+});
 
 server.listen(port, () => {
     console.log('HTTPS is running on port ' + port + '')
