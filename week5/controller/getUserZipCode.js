cbe28a2bdd0aa502417f640c07deb6e95684a3d8
leemgzu <92341784+leemgzu@users.noreply.github.com>
2023-10-05 19:13:16 -0700

    week 5 bug fix

--- week5/controller/getUserZipCode.js
+++ week5/controller/getUserZipCode.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const apiKey = process.env.API_KEY_IPLOCATION;
+
 
 export const getUserZipCode = async (userIP) => {
-    const locationUrl = 'https://api.ip2location.io/?key=${apiKey}&ip=${userIP}';
+    const apiKey = process.env.API_KEY_IPLOCATION;
+    const locationUrl = `https://api.ip2location.io/?key=${apiKey}&ip=${userIP}`;
     const locationResponse = await axios.get(locationUrl); //bc axios is an async library
     console.log(locationResponse?.data);
     return {"zip_code": locationResponse?.data?.zip_code}; //use ? to handle undefine
-}
+}   
