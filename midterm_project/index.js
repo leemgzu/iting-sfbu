347158871098ee4ee66bfa020f868b90aeb36c5c
leemgzu <92341784+leemgzu@users.noreply.github.com>
2023-10-21 18:09:02 -0700

    Midterm project

--- midterm_project/index.js
+++ midterm_project/index.js
@@ -1,7 +1,9 @@
 import https from 'https';
 import fs from 'fs';
 import express from 'express';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
+import {getTrackingNumber, getTrackingInfo} from "./route/getTracking.js"
+
 
 const app = express();
 const port = 8080;
@@ -10,15 +12,38 @@ const httpsOptions = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem')
 }
-// dotenv.config();
+dotenv.config();
 
 const server = https.createServer(httpsOptions,app);  
-const logger = getLoggerInstance();
+// const logger = getLoggerInstance();
 
 app.use(express.json());
 
+app.post('/tracking_detail', async (req, res) => {
+    const orderNumber = req.body.orderNumber;
+    if (orderNumber != null) {
+        const trackingNumber = getTrackingNumber(orderNumber);
+        if (trackingNumber != undefined) {
+            const trackingInfo = await getTrackingInfo(trackingNumber)
+            if ( trackingInfo !== undefined){
+                res.send(trackingInfo);
+            }
+            else {
+                res.status(400).send("Can not find tracking information from TrackMore.");
+            }
+        }
+        else {
+            res.status(400).send("Can not find tracking number!");
+        }
+    }
+    else {
+        res.status(400).send("Order number not found!");
+    }
+});
+
 
 server.listen(port, ()=> {
-    logger.info(`Server is listening on port ${port}!`);
+    // logger.info(`Server is listening on port ${port}!`);
+    console.log('server is listening...');
 }) 
     
---
+++ midterm_project/testData.js
@@ -0,0 +1,25 @@
+export const info =[{
+    orderNumber: "1234",
+    trackingNumer: "9261290312833844954982",
+},
+{
+    orderNumber: "123456789",
+    trackingNumer: "6860010537552",
+},{
+    orderNumber: "123456789",
+    trackingNumer: "6860010537552",
+},{
+    orderNumber: "123456789",
+    trackingNumer: "6860010537552",
+},{
+    orderNumber: "123456789",
+    trackingNumer: "6860010537552",
+},{
+    orderNumber: "123456789",
+    trackingNumer: "123456789",
+},{
+    orderNumber: "123456789",
+    trackingNumer: "123456789",
+},
+];
+
